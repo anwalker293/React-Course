a1858ed82deb395c729635e1e55e6dca8575fd90
Alex Walker <alex.walker@indicio.tech>
2023-01-12 22:05:13 +0100

    feat: scratch
    Signed-off-by: Alex Walker <alex.walker@indicio.tech>

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,72 +1,108 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 import "./App.css";
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
+  const [searchField, setSearchField] = useState("");
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilterMonsters] = useState(monsters);
+  console.log({ searchField });
 
-    this.state = {
-      monsters: [],
-      searchField: "",
-    };
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) =>
-        this.setState(
-          () => {
-            return { monsters: users };
-          },
-          () => {
-            console.log(this.state);
-          }
-        )
-      );
-  }
+      .then((users) => setMonsters(users));
+  }, []); // callback/effect, state vals or prop vals
+  // empty, don't trigger callback, just call fetch onMount
+  // this function should never be called again except for
+  // when the function mounts
 
-  // Learn how to increase performance,
-  // even if only for one function
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLocaleLowerCase();
-    // Best practice is to use non-modifying methods
-
-    this.setState(() => {
-      return { searchField };
-    });
-  };
-
-  render() {
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
-
-    const filteredMonsters = monsters.filter((monster) => {
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
       return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
-    return (
-      <div className="App">
-        <h1 className="app-title">Monsters Rolodex</h1>
-        {/* {filteredMonsters.map((monster) => {
+    setFilterMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+  };
+
+  // Only want to filterMonsters when things relevant to it change
+  const filteredMonsters = monsters.filter((monster) => {
+    return monster.name.toLocaleLowerCase().includes(searchField);
+  });
+
+  return (
+    <div className="App">
+      <h1 className="app-title">Monsters Rolodex</h1>
+      {/* {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>
           );
         })} */}
-        <SearchBox
-          className="search-box"
-          onChangeHandler={onSearchChange}
-          placeholder="search monsters"
-        />
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
-}
+      <SearchBox
+        className="search-box"
+        onChangeHandler={onSearchChange}
+        placeholder="search monsters"
+      />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
+
+//// class App extends Component {
+//  constructor() {
+//    super();
+//
+//    this.state = {
+//      monsters: [],
+//      searchField: "",
+//    };
+//  }
+//
+//  componentDidMount() {
+//    fetch("https://jsonplaceholder.typicode.com/users")
+//      .then((response) => response.json())
+//      .then((users) =>
+//        this.setState(
+//          () => {
+//            return { monsters: users };
+//          },
+//          () => {
+//            console.log(this.state);
+//          }
+//        )
+//      );
+//  }
+//
+//  // Learn how to increase performance,
+//  // even if only for one function
+//  onSearchChange = (event) => {
+//    const searchField = event.target.value.toLocaleLowerCase();
+//    // Best practice is to use non-modifying methods
+//
+//    this.setState(() => {
+//      return { searchField };
+//    });
+//  };
+//
+//  render() {
+//    const { monsters, searchField } = this.state;
+//    const { onSearchChange } = this;
+//
+//    const filteredMonsters = monsters.filter((monster) => {
+//      return monster.name.toLocaleLowerCase().includes(searchField);
+//    });
+//
+//    return (
+//         );
+//  }
+//}
 
 export default App;
