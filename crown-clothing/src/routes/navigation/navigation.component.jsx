3186ad9881cbc143e9176044f7bc4563a7c136a1
Alex Walker <alex.walker@indicio.tech>
2023-01-15 17:57:16 +0100

    fix: major major refactor
    Signed-off-by: Alex Walker <alex.walker@indicio.tech>

--- crown-clothing/src/routes/navigation/navigation.component.jsx
+++ crown-clothing/src/routes/navigation/navigation.component.jsx
@@ -4,26 +4,39 @@ import { Outlet, Link } from "react-router-dom";
 import { UserContext } from "../../contexts/user.context";
 
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 import "./navigation.styles.scss";
 
 const Navigation = () => {
   const { currentUser, setCurrentUser } = useContext(UserContext);
 
-  console.log(currentUser);
+  const signOutHandler = async () => {
+    await signOutUser();
+    setCurrentUser(null);
+  };
+
   return (
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to="/">
           <CrwnLogo className="logo" />
         </Link>
-        <div className="links-container">
-          <Link className="nav-links-container" to="/shop">
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
             SHOP
           </Link>
-          <Link className="nav-links-container" to="/auth">
-            Sign in
-          </Link>
+
+          {currentUser ? (
+            <span className="nav-link" onClick={signOutHandler}>
+              {" "}
+              SIGN OUT{" "}
+            </span>
+          ) : (
+            <Link className="nav-link" to="/auth">
+              SIGN IN
+            </Link>
+          )}
         </div>
       </div>
       <Outlet />
